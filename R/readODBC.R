d4a3ebd2bb3c8e3defd59f7f22f3cf124a9266e8
Chasco <chascob@oregonstate.edu>
2021-04-13 20:20:05 -0700

    Second test

---
+++ R/readODBC.R
@@ -0,0 +1,9 @@
+#'Read in information from database using 
+#'
+#'RODBC
+#'@param x some parameter we are interested in
+#'@return A table of information
+#'@export
+readRODBC <- function(){
+  return();
+}
