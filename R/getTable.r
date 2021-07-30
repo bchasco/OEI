e62e7dbdec54d9c61c1a4b7ad60128a2eb24192b
Chasco <chascob@oregonstate.edu>
2021-07-30 11:01:29 -0700

    Updated manual.

--- R/getTable.r
+++ R/getTable.r
@@ -2,27 +2,28 @@
 #'
 #'getTable
 #'@param uid User ID
-#'@param pwd Default passwoed
+#'@param pwd Default password
 #'@param db Database name
-#'@return table A table from the list of queries that have already been built.
+#'@return table A table from the list of tables that have already been built.
 #'@export
 getTable <- function(uid = uid,
                     pwd = pwd,
-                    db = db,
+                    db = "NWFSC_OCEAN",
                     tableName = NA){
   
-  #Create the channel first
-  channel <- odbcConnect("NWFSC_OCEAN", uid=uid, pwd=pwd)
+  #Create the channel first. Notice we do not use library. The library is already "install"
+  #in the description file. And you use :: to reference the function in RODBC
+  channel <-   RODBC::odbcConnect("NWFSC_OCEAN", uid=uid, pwd=pwd)
   
   #Available queries 
-  availableTables <- getTableList()
+  availableTables <- RODBC::sqlTables(channel = channel)
   
   #Read the available query names in the database
-  if(qryName%in%availableTables){
-    table <- readTable(channel, tableName)
+  if(tableName%in%availableTables){
+    table <- RODBC::sqlFetch(channel, tableName)
     return(table)
   }else{
-    cat("The are the available queries for your user account. \n Please choose one.")
-    cat(table)
+    cat("These are the available table for your user account. \n Please choose one.")
+    cat(availableTables)
   }
 }
