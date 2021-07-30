6985498a070df658177b56bd0d836c385a39b8b2
Brandon Chasco <brandon.chasco@gmail.com>
2021-07-30 09:56:57 -0700

    Update connectDb.r
    Add the parameters for the function

--- R/connectDb.r
+++ R/connectDb.r
@@ -1,1 +1,14 @@
-
+#'Read in information from database using 
+#'
+#'RODBC
+#'@param uid User ID
+#'@param pwd Default passwoed
+#'@param db Database name
+#'@return channel A channel that can be passed to other functions that allows you to query the database
+#'@export
+connectDb <- function(uid = default, 
+                      pwd = default, 
+                      db = default){
+  
+  return();
+}
