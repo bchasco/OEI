d079a0d2621ef335c09c34306389fb1b1e9abf01
Brandon Chasco <brandon.chasco@gmail.com>
2021-07-30 10:02:19 -0700

    Update getTable.r

--- R/getTable.r
+++ R/getTable.r
@@ -1,1 +1,9 @@
-
+getQuery <- function(uid = uid,
+                    pwd = pwd,
+                    db = db,
+                    qryName = "SST"){
+  
+  #Create the channel first
+  channel <- odbcConnect("NWFSC_OCEAN", uid=uid, pwd=pwd)
+  #  
+}
