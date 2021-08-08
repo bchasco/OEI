4974248167952325ec9c00a4c2411849d6bcf33a
Chasco <brandon.chasco@gmail.com>
2021-08-08 12:29:54 -0700

    Comments and new arguments for the plot

--- R/plot.r
+++ R/plot.r
@@ -6,12 +6,23 @@
 #' @export
 #'
 #' @examples plot
-plot <- function(df=data.frame(x=1:4,y=1:4, c= NULL)){
-  p <- ggplot2::ggplot(data=as.data.frame(df),ggplot2::aes(x=x,y=y)) +
-    ggplot2::geom_line()
+plot <- function(df=data.frame(x=1:4,y=1:4, c= NULL),
+                 ylab="",
+                 xlab=""){
+  
+  #ggplot object
+  p <- ggplot2::ggplot(df,ggplot2::aes(x=x,y=y)) +
+    ggplot2::geom_point() +
+    ggplot2::theme_bw() +
+    ggplot2::ylab(ylab) +
+    ggplot2::xlab(xlab)
+  
+  #Categorical data
   if(!is.null(df$c)){
     p <- p + 
-      ggplot2::facet_wrap(~c)
+      ggplot2::facet_wrap(~c, ncol=1)
   }
+  
+  #Return the plot
   print(p)
 }
