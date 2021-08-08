b8143979be1caf236e50c4bf32da89b04e0a3c47
Chasco <brandon.chasco@gmail.com>
2021-08-08 11:43:30 -0700

    UPdates to plot and vignettes

---
+++ R/plot.r
@@ -0,0 +1,17 @@
+#' Title
+#'
+#' @param df data.frame
+#'
+#' @return ggplot
+#' @export
+#'
+#' @examples plot
+plot <- function(df=data.frame(x=1:4,y=1:4, c= NULL)){
+  p <- ggplot2::ggplot(data=as.data.frame(df),ggplot2::aes(x=x,y=y)) +
+    ggplot2::geom_line()
+  if(!is.null(df$c)){
+    p <- p + 
+      ggplot2::facet_wrap(~c)
+  }
+  print(p)
+}
