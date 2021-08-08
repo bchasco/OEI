5e10fd3f3156bee6c2b54cece77636d6d35f872a
Chasco <brandon.chasco@gmail.com>
2021-08-08 12:29:54 -0700

    Quick change to plot

--- R/plot.r
+++ R/plot.r
@@ -8,10 +8,11 @@
 #' @examples plot
 plot <- function(df=data.frame(x=1:4,y=1:4, c= NULL)){
   p <- ggplot2::ggplot(data=as.data.frame(df),ggplot2::aes(x=x,y=y)) +
-    ggplot2::geom_line()
+    ggplot2::geom_point() +
+    ggplot2::theme_bw()
   if(!is.null(df$c)){
     p <- p + 
-      ggplot2::facet_wrap(~c)
+      ggplot2::facet_wrap(~c, ncol=1)
   }
   print(p)
 }
