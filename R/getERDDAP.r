f7cf19af1497b7eae4859f4dd3ec79b471e16e5e
bchasco <brandon.chasco@gmail.com>
2022-02-14 15:50:19 -0800

    Var names update.

--- R/getERDDAP.r
+++ R/getERDDAP.r
@@ -1,14 +1,22 @@
 #'Get an ERDDAP file
-#'getERDAPP
 #'@param var=NULL Dataset ID for an erddap file (see 'https://oceanview.pfeg.noaa.gov/erddap/info/index.html?page=1&itemsPerPage=1000')  
 #'@return ERDDAP data.frame
+#'@examples #if you don't know the name of an erddap fileset just type
+#'@examples #and you'll be prompted to choose a fileset
+#'@examples getERDDAP()
+#'@examples #you can either enter the row number that you're interested in 
+#'@examples #or you can type something like 'PDO', and it will search the list for the PDO filesets
+#'@examples #Alternatively,
+#'@examples #If you don't want to look through all of the file sets, you just type
 #'@examples x <- getERDDAP(var='cciea')
-#' @export getERDDAP
+#'@examples #and you'll be asked to choose a row from the listed cciea filesets
+#'@export getERDDAP
 getERDDAP <- function(var = NULL){
   #Read in the table contents
   content <- rvest::read_html("https://oceanview.pfeg.noaa.gov/erddap/info/index.html?page=1&itemsPerPage=1000")
   tableDAP <- content %>% rvest::html_table(fill = TRUE)
   dataID <- as.data.frame(tableDAP[[2]]$'Dataset ID')
+  print("test")
   if(is.null(var)){
     print(dataID)
     var <- readline(prompt = "Enter any number or grep a name: ");
@@ -27,10 +35,20 @@ getERDDAP <- function(var = NULL){
   dataSet <- tableDAP[[2]]$'Dataset ID'[varVal]
   title <- tableDAP[[2]]$'Title'[varVal]
   
-  cat("You are collecting data for: \n",title)
+  cat("You are collecting test data for: \n",title)
   cat("\nThe 'Dataset ID' for this data is :\n",dataSet,"\n")
   
   dataSet <- tableDAP[[2]]$'Dataset ID'[varVal]
+
+  varDAP <- rvest::read_html(paste0("https://oceanview.pfeg.noaa.gov/erddap/info/",dataSet,"/index.html"))
+  varTable <- varDAP %>% rvest::html_table(fill = TRUE)
+  varNames <- unique(varTable[[3]]$`Variable Name`)
+  
+  
+  print(t(c("All",varNames[varNames!="NC_GLOBAL"])))
+  #varVal <- readline(prompt = "Choose your variables (not functional yet):");
+  print("Choose your variables (not functional yet):")
+  
   dat <- read.csv(paste0("https://oceanview.pfeg.noaa.gov/erddap/tabledap/",dataSet,".csv?"), 
                   header = TRUE, 
                   stringsAsFactors = FALSE)
